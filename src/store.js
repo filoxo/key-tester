6d1cf3e625d03591e19dc0d8475299335142cf81
Carlos Filoteo <cfiloteo@ancestry.com>
2017-12-12 22:39:58 -0700

    Add default detectedKey map, add getDetectedKey getter

--- src/store.js
+++ src/store.js
@@ -6,12 +6,21 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
   state: {
     detectedKeys: {
-      '65': false // A
+      20: false, // CAPS
+      65: false, // A
+      83: false, // S
+      68: false, // D
+      70: false // F
     }
   },
   mutations: {
     setActive (state, keyCode) {
       state.detectedKeys[keyCode] = true
+    }
+  },
+  getters: {
+    getDetectedKey: state => which => {
+      return state.detectedKeys[which]
     }
   }
 })
