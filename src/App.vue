7a6020aa7a5c087202141038d6e2f65001199db7
Carlos Filoteo <cfiloteo@ancestry.com>
2017-12-13 10:49:47 -0700

    Dynamically create rows, keys based on stored map

--- src/App.vue
+++ src/App.vue
@@ -7,69 +7,8 @@
       <div>
         <Testarea />
       </div>
-      <div class="row">
-        <Key label="`" />
-        <Key label="1" />
-        <Key label="2" />
-        <Key label="3" />
-        <Key label="4" />
-        <Key label="5" />
-        <Key label="6" />
-        <Key label="7" />
-        <Key label="8" />
-        <Key label="9" />
-        <Key label="0" />
-        <Key label="-" />
-        <Key label="=" />
-        <Key label="DEL" size="1.5" />
-      </div>
-      <div class="row">
-        <Key label="TAB" size="1.5" />
-        <Key label="Q" />
-        <Key label="W" />
-        <Key label="E" />
-        <Key label="R" />
-        <Key label="T" />
-        <Key label="Y" />
-        <Key label="U" />
-        <Key label="I" />
-        <Key label="O" />
-        <Key label="P" />
-        <Key label="[" />
-        <Key label="]" />
-        <Key label="\" />
-      </div>
-      <div class="row">
-        <Key label="CAPS" :is-active=this.detectedKeys.CapsLock size="2.25" />
-        <Key label="A" :is-active=this.detectedKeys.KeyA />
-        <Key label="S" :is-active=this.detectedKeys.KeyS />
-        <Key label="D" :is-active=this.detectedKeys.KeyD />
-        <Key label="F" :is-active=this.detectedKeys.KeyF />
-        <Key label="G" :is-active=this.detectedKeys.KeyG />
-        <Key label="H" :is-active=this.detectedKeys.KeyH />
-        <Key label="J" :is-active=this.detectedKeys.KeyJ />
-        <Key label="K" :is-active=this.detectedKeys.KeyK />
-        <Key label="L" :is-active=this.detectedKeys.KeyL />
-        <Key label=";" :is-active=this.detectedKeys.Semicolon />
-        <Key label="'" :is-active=this.detectedKeys.Quote />
-        <Key label="ENTER" :is-active=this.detectedKeys.Enter size="2.25" />
-      </div>
-      <div class="row">
-        <Key label="SHIFT" size="3.25" />
-        <Key label="Z" />
-        <Key label="X" />
-        <Key label="C" />
-        <Key label="V" />
-        <Key label="B" />
-        <Key label="N" />
-        <Key label="M" />
-        <Key label="," />
-        <Key label="." />
-        <Key label="/" />
-        <Key label="SHIFT" size="3.25" />
-      </div>
-      <div style="margin-top: 50px;">
-        <div class="row" v-for="(row, index) in map.rows" :key=index>
+      <div>
+        <div class="row" v-for="(row, index) in map.rows" :key=index >
           <Key
             v-for="key in row"
             :key=key.code
@@ -118,7 +57,7 @@ header {
   height: 56px;
   padding: 0 16px 0 24px;
   background-color: steelblue;
-  color: #ffffff;
+  color: #fff;
 }
 
 header span {
