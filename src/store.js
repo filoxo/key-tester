5241c2ac17d58e7a4e5837d74f43f17e5677e96b
Carlos Filoteo <cfiloteo@ancestry.com>
2017-12-13 10:48:45 -0700

    Import apple keymap, set initial state

--- src/store.js
+++ src/store.js
@@ -1,45 +1,21 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import map from './keymaps/apple'
 
 Vue.use(Vuex)
 
+let detectedKeys = []
+
+map.rows.forEach(row => {
+  row.forEach(keyDef => {
+    detectedKeys[keyDef.code] = false
+  })
+})
+
 const store = new Vuex.Store({
   state: {
-    detectedKeys: {
-      'CapsLock': false, //
-      'KeyA': false,
-      'KeyS': false,
-      'KeyD': false,
-      'KeyF': false,
-      'KeyG': false,
-      'KeyH': false,
-      'KeyI': false,
-      'KeyJ': false,
-      'KeyK': false,
-      'KeyL': false,
-      'Semicolon': false,
-      'Quote': false,
-      'Enter': false
-    },
-    map: {
-      rows: [
-        [
-          {code: 'CapsLock', label: 'CAPS', size: '2.25'},
-          {code: 'KeyA', label: 'A'},
-          {code: 'KeyS', label: 'S'},
-          {code: 'KeyD', label: 'D'},
-          {code: 'KeyF', label: 'F'},
-          {code: 'KeyG', label: 'G'},
-          {code: 'KeyH', label: 'H'},
-          {code: 'KeyJ', label: 'J'},
-          {code: 'KeyK', label: 'K'},
-          {code: 'KeyL', label: 'L'},
-          {code: 'Semicolon', label: ';'},
-          {code: 'Quote', label: '\''},
-          {code: 'Enter', label: 'ENTER', size: '2.25'}
-        ]
-      ]
-    }
+    detectedKeys: {...detectedKeys},
+    map
   },
   mutations: {
     setActive (state, keyCode) {
