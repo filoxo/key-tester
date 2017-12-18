c4e9b6adcbc58cffe5aa6332e47b1a62b7356964
Carlos Filoteo <c.filoteo@live.com>
2017-12-17 21:49:03 -0700

    Focus on textarea when Testarea is mounted

--- src/components/Testarea.vue
+++ src/components/Testarea.vue
@@ -1,10 +1,21 @@
 <template>
-  <textarea class="testarea" name="testarea" id="testarea" cols="30" rows="3" placeholder="Begin typing..." v-on:keydown="detectKey"></textarea>
+  <textarea
+    class="testarea"
+    name="testarea"
+    id="testarea"
+    cols="30"
+    rows="3"
+    placeholder="Begin typing..."
+    ref="testarea"
+    @keydown="detectKey" />
 </template>
 
 <script>
 export default {
   name: 'testarea',
+  mounted () {
+    this.$refs.testarea.focus()
+  },
   methods: {
     detectKey (e) {
       console.log(e.code)
